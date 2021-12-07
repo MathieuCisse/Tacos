1cd547f84760816550548a52045925f7cac066bc
Mathieu C <mathieu.cisse@cirkwi.com>
2021-12-07 12:18:03 +0100

    Système migration

---
+++ Tacos/Migrations/20211207103813_systemeIngredient.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tacos.Migrations
+{
+    public partial class systemeIngredient : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
